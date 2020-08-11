ef1c4ea9b2bcca042c74e6723b7fbbcea43cc589
bakaflan <184444280@qq.com>
2020-08-12 02:24:20 +0800

    [ruiilng] feat: fix config bug

--- src/component/Eductions.js
+++ src/component/Eductions.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Eduction from "./Eduction";
-import "../styles/eductions.css";
+import React from 'react';
+import Eduction from './Eduction';
+import '../styles/eductions.css';
 
 class Eductions extends React.Component {
   constructor(props) {
@@ -9,30 +9,30 @@ class Eductions extends React.Component {
       eductions: [
         {
           id: 1,
-          year: "1990",
-          summary: "I was born in Katowice",
+          year: '1990',
+          summary: 'I was born in Katowice',
           para:
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi.",
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi.',
         },
         {
           id: 2,
-          year: "2005",
-          summary: "Secondary school specializing in artistic",
+          year: '2005',
+          summary: 'Secondary school specializing in artistic',
           para:
-            "Eos, explicabo, nam, tenetur et ab eius deserunt aspernatur ipsum ducimus quibusdam quis voluptatibus.",
+            'Eos, explicabo, nam, tenetur et ab eius deserunt aspernatur ipsum ducimus quibusdam quis voluptatibus.',
         },
         {
           id: 3,
-          year: "2009",
-          summary: "First level graduation in Graphic Design",
+          year: '2009',
+          summary: 'First level graduation in Graphic Design',
           para:
-            "Aspermatur, mollitia, quos maxime eius suscipit sed beatae ducimus quaerat quibusdam perferendis? lusto, quibusdam asperiores unde",
+            'Aspermatur, mollitia, quos maxime eius suscipit sed beatae ducimus quaerat quibusdam perferendis? lusto, quibusdam asperiores unde',
         },
         {
           id: 4,
-          year: "2012",
-          summary: "Second level graduation in Graphic Design",
-          para: "Ducimus, aliquam tempore autem itaque et accusantium!",
+          year: '2012',
+          summary: 'Second level graduation in Graphic Design',
+          para: 'Ducimus, aliquam tempore autem itaque et accusantium!',
         },
       ],
     };
@@ -42,8 +42,8 @@ class Eductions extends React.Component {
     return (
       <div className="eductions">
         <h1>Eduction</h1>
-        {this.state.eductions.map((eduction,index) => (
-          <Eduction key = {index} info={eduction} />
+        {this.state.eductions.map((eduction, index) => (
+          <Eduction id={index} key={index} info={eduction} />
         ))}
       </div>
     );
